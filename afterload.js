6f9396a45ba44856818bb24cf34c61f44f95fc0c
srLacky <shivakris19@gmail.com>
2026-08-06 14:18:24 +0800

    to be sorted later

---
+++ afterload.js
@@ -0,0 +1,6 @@
+const headerLogo = document.getElementById("header-logo")
+headerLogo.addEventListener("click", () => {
+    window.location.href = "home.html"
+})
+
+const headerShiva = document.gete
